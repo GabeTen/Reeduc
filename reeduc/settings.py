22a62cca6f4118bc35ded2304ef969002ceddb51
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-10-19 20:13:37 -0300

    feature: adicionando logo

--- reeduc/settings.py
+++ reeduc/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Suas apps
     'core',
     'cursos',
 ]
@@ -119,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' # onde está a pasta com as imagens
+
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static",  # onde está a pasta com as imagens
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
