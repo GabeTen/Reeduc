2efd22bd0a4f5b8b7296c7286c8aad96092427a0
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-10-24 22:40:38 -0300

    refactor(routes): adiciona prefixo 'publicacoes/' às rotas de criação, edição e exclusão

--- educacao/urls.py
+++ educacao/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     # exemplo: rota para listar publicações educacionais
     path('publicacoes/', views.publicacao_list, name='publicacoes_list'),
-    path('create/', views.create_publicacao, name='create_publicacao'),
-    path('<int:pk>/edit/', views.edit_publicacao, name='edit_publicacao'),
-    path('<int:pk>/delete/', views.delete_publicacao, name='delete_publicacao'),
+    path('publicacoes/create/', views.create_publicacao, name='create_publicacao'),
+    path('publicacoes/<int:id>/edit/', views.edit_publicacao, name='edit_publicacao'),
+    path('publicacoes/<int:id>/delete/', views.delete_publicacao, name='delete_publicacao'),
+
 ]
