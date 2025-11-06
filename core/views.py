4da77909569a1172526bcf2e0c5bca0c70dbf2da
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-11-06 18:36:18 -0300

    feat: adicionando logica de login  via AJAX

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from cursos.models import Course
+from educacao.models import PublicacaoEducacional, User
+from reeduc.utils import get_form_errors_as_json
 
 
 def home(request):
@@ -25,10 +29,12 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('dashboard')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'core/login.html', {'form': form})
+            return JsonResponse({'success': True, 'redirect_url': '/dashboard/'})
+        else:
+            errors = get_form_errors_as_json(form)
+            return JsonResponse({'success': False, 'errors': errors}, status=400)
+
+    return render(request, 'core/login.html', {'form': AuthenticationForm()})
 
 def logout_view(request):
     logout(request)
