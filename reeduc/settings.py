e635d418e40ea534e24c447fdb543e7388ad3ed6
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-11-15 18:07:30 -0300

    Removendo RolePermissionsMiddleware do settings
    Removido RolePermissionsMiddleware da lista de middleware.

--- reeduc/settings.py
+++ reeduc/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'rolepermissions.middleware.RolePermissionsMiddleware',   # 🔥 AQUI
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
