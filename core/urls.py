0c77111d8a4d0ba6c68b67bfb6d43278afd4a08f
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2025-11-15 22:50:21 -0300

    feat(back+front): adicionando template + view do dashboard administravo

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('students/filter_by_select2/json/', views.filter_students_by_select2, name='filter_students_by_select2')
-
+    path('students/filter_by_select2/json/', views.filter_students_by_select2, name='filter_students_by_select2'),
+    path('users/data/', views.users_data, name="users_data"),
+    path('users/page/', views.users_page, name="users_page"),
+    path('users/<int:id>/edit/role/', views.edit_role_user, name="edit_role_user")
 ]
